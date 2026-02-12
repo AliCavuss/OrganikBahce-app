259dc321505c0ec1881e266d495aad02eab89b2e
AliCavuss <147621323+AliCavuss@users.noreply.github.com>
2026-02-12 19:40:01 +0300

    Add project files.

---
+++ Admin.Mvc/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Mvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult Approve(int id)
+        {
+            TempData["Success"] = $"Kullanıcı #{id} için satıcı onayı verildi .";
+            return RedirectToAction("List", "User");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# OrganikBahce
+
+Proje
+
+
+
+Üreticiden tüketiciye ata tohumlarından üretilen organik ürünlerin satışı hedeflenmektedir.
+
