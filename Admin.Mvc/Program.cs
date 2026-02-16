e5e2ce4fa9c1cff093d25376f473ea15c739c955
sblcbc <sbl.cebeci@gmail.com>
2026-02-16 13:42:26 +0300

    ConnectionString'ler yazıldı. App.Data, proje referansı olarak 2 MVC^ye eklendi. Program.cs'lere DbContext eklendi.

--- Admin.Mvc/Program.cs
+++ Admin.Mvc/Program.cs
@@ -1,7 +1,16 @@
+using App.Data;                  
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<OrganikBahceDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
 
 var app = builder.Build();
 
--- OrganikBahce.MVC/Program.cs
+++ OrganikBahce.MVC/Program.cs
@@ -1,11 +1,18 @@
 using App.Data;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<OrganikBahceDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+
 
 var app = builder.Build();
 
