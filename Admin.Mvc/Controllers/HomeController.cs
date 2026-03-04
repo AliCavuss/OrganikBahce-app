b46fcd86111b140e28e48b6b273613beba6b3d77
AliCavuss <147621323+AliCavuss@users.noreply.github.com>
2026-03-04 13:31:51 +0300

    Admin.mvc Update

--- Admin.Mvc/Controllers/HomeController.cs
+++ Admin.Mvc/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Admin.Mvc.Models;
+using App.Data.Context;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,13 +7,20 @@ namespace Admin.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-      
+        private readonly OrganikBahceDbContext _db;
+
+        public HomeController(OrganikBahceDbContext db)
+        {
+            _db = db;
+        }
 
         public IActionResult Index()
         {
+            
+            ViewBag.CategoryCount = _db.Categories.Count();
+            ViewBag.ProductCount = _db.Products.Count();
+
             return View();
         }
-
-        
     }
 }
--- Admin.Mvc/Models/ViewModels/CategoryEditViewModel.cs
+++ Admin.Mvc/Models/ViewModels/CategoryEditViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace Admin.Mvc.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Admin.Mvc.Models.ViewModels
 {
     public class CategoryEditViewModel
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Kategori adı zorunludur.")]
+        [StringLength(100, ErrorMessage = "Kategori adı en fazla 100 karakter olabilir.")]
         public string Name { get; set; } = null!;
+
+        [StringLength(20, ErrorMessage = "Color en fazla 20 karakter olabilir.")]
         public string? Color { get; set; }
+
+        [StringLength(100, ErrorMessage = "IconCssClass en fazla 100 karakter olabilir.")]
         public string? IconCssClass { get; set; }
     }
 }
