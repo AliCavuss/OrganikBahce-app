bdea95b56d0a411b1c856840afd92185863e903b
sblcbc <sbl.cebeci@gmail.com>
2026-03-06 22:26:18 +0300

    AdminRevize

--- Admin.Mvc/Controllers/CommentController.cs
+++ Admin.Mvc/Controllers/CommentController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Data.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Admin.Mvc.Controllers
 {
     public class CommentController : Controller
     {
+        private readonly OrganikBahceDbContext _db;
+
+        public CommentController(OrganikBahceDbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("/comment")]
+        [HttpGet]
         public IActionResult List()
         {
             return View();
         }
+
+        [Route("/comment/{commentId:int}/approve")]
+        [HttpGet]
         public IActionResult Approve(int id)
         {
             ViewBag.Id = id;
--- Admin.Mvc/Controllers/UserController.cs
+++ Admin.Mvc/Controllers/UserController.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using App.Data.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Admin.Mvc.Controllers
 {
     public class UserController : Controller
     {
+        private readonly OrganikBahceDbContext _db;
+
+        public UserController   (OrganikBahceDbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("/users")]
+        [HttpGet]
         public IActionResult List()
         {
             return View();
         }
 
+        [Route("/users/{userId:int}/approve")]
+        [HttpGet]
         public IActionResult Approve(int id)
         {
             TempData["Success"] = $"Kullanıcı #{id} için satıcı onayı verildi .";
