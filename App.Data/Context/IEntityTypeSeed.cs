9a3325ac16676b22f38149c219a807bc487cc439
sblcbc <sbl.cebeci@gmail.com>
2026-03-17 08:03:58 +0300

    Data

---
+++ App.Data/Context/IEntityTypeSeed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace App.Data.Context
+{
+    public interface IEntityTypeSeed<T>where T : class
+    {
+        void SeedData(EntityTypeBuilder<T> builder);
+    }
+}
